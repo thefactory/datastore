1d555c3317f11fa4c61e07e6eb5316c95e06e99c
Peter Teichman <peter@thefactory.com>
2013-10-18 15:19:50 -0700

    Add default TabletOptions and a convenient constructor
    Test Plan: upcoming serialization daemon
    
    Reviewers: tberman, jyeo, stephan
    
    Reviewed By: stephan
    
    CC: stephan, jimmy, justin, jyeo, tberman
    
    Differential Revision: http://phabricator.thefactory.com/D856

--- java/src/main/java/com/thefactory/datastore/TabletOptions.java
+++ java/src/main/java/com/thefactory/datastore/TabletOptions.java
@@ -4,4 +4,14 @@ public class TabletOptions {
     public int blockSize;
     public boolean useCompression;
     public int keyRestartInterval;
+
+    public TabletOptions() {
+        this(4096, true, 16);
+    }
+
+    public TabletOptions(int blockSize, boolean useCompression, int keyRestartInterval) {
+        this.blockSize = blockSize;
+        this.useCompression = useCompression;
+        this.keyRestartInterval = keyRestartInterval;
+    }
 }
