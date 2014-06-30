de4ed9092d24a322ca73b08df3ff93d3c7d841de
Paul Betts <paul@paulbetts.org>
2014-06-30 11:38:28 -0700

    Add a class to store methods to fix brain-dead WinRT-based filesystem abstraction

---
+++ csharp/TheFactory.Datastore/src/PclStorageShims.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PCLStorage;
+
+namespace TheFactory.Datastore {
+    static class PclStorageShims {
+        public static async Task<IFolder> CreateDirectoryRecursive(this IFileSystem fs, string path) {
+            var currentFolder = default(IFolder);
+            var currentPath = path;
+
+            var separators = new[] { '\\', '/' };
+            while ((currentFolder = await fs.GetFolderFromPathAsync(currentPath)) == null) {
+                var idx = currentPath.LastIndexOfAny(separators);
+                if (idx == -1) {
+                    throw new ArgumentException("Can't find base folder or path is malformed", "path");
+                }
+
+                currentPath = currentPath.Substring(0, idx);
+            }
+
+            var toCreate = path.Replace(currentPath, "").Split('\\', '/');
+            foreach (var v in toCreate) {
+                currentFolder = await currentFolder.CreateFolderAsync(v, CreationCollisionOption.OpenIfExists);
+            }
+
+            return currentFolder;
+        }
+    }
+}
